b02a6479becced5899df0c18ed5410256d59654e
Diego Cruz <diegoogc@gmail.com>
2023-11-12 17:06:27 -0300

    Finalizado QA Desafio

--- src/test/java/in/reqres/qaTestSalsa/APITestes.java
+++ src/test/java/in/reqres/qaTestSalsa/APITestes.java
@@ -2,14 +2,10 @@ package in.reqres.qaTestSalsa;
 
 
 import static org.hamcrest.Matchers.*;
-import static io.restassured.RestAssured.*;
-import static com.jayway.restassured.module.jsv.JsonSchemaValidator.*;
-
-
 import java.io.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
+import org.json.simple.JSONObject;
 import com.jayway.restassured.module.jsv.JsonSchemaValidator;
 
 import io.restassured.RestAssured;
@@ -32,12 +28,13 @@ public class APITestes {
 			    .getResourceAsStream ("listuserjsonschema.json");
 		
 		RestAssured.given()
-	    .when()
-	      .get("api/users?page=2")
-	    .then()
-	      .statusCode(200)
-	      .assertThat()
-	      .body(JsonSchemaValidator.matchesJsonSchema(listUserJsonSchema));
+		 .param("page", "2")
+	    	.when()
+	     .get("api/users")
+	      	.then()
+	     .statusCode(200)
+	     .assertThat()
+	     .body(JsonSchemaValidator.matchesJsonSchema(listUserJsonSchema));
 	}
 	
 	
@@ -48,14 +45,42 @@ public class APITestes {
      .body("{\"email\": \"eve.holt@reqres.in\",\"password\": \"cityslicka\"}")
      		
      .contentType(ContentType.JSON)
-        .when()
-     .post("/api/login")
-        .then().assertThat()
-     .statusCode(200)
-     .body(containsString("token"))
-     .and()
-	 .body("token", notNullValue());
+     		.when()
+     	.post("/api/login")
+        	.then()
+        .statusCode(200)
+        .assertThat()
+        .body(containsString("token"))
+        .and()
+        .body("token", notNullValue());
 
 	}
 
+	
+	@Test
+	public void deveCriarNovoUsuario() {
+		
+		JSONObject requestParams = new JSONObject();
+        requestParams.put("name", "morpheus");
+        requestParams.put("job", "leader");
+		
+		RestAssured.given()
+	     .body(requestParams.toJSONString())
+	     		
+	     .contentType(ContentType.JSON)
+	        .when()
+	     .post("/api/users")
+	        .then()
+	     .statusCode(201)
+		 .assertThat()
+		 .body(containsString("name"))
+		 .and()
+		 .body(containsString("job"))
+		 .and()
+		 .body(containsString("id"))
+		 .and()
+		 .body(containsString("createdAt"));
+	}
+	
+	
 }
